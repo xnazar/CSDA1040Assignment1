a3f30784f0463bb524b5a199a7030076f019f1cb
Xenel Nazar <xen.nazar@gmail.com>
2020-05-30 13:08:17 -0400

    change theme

--- ASSIGNMENT_1-GROUP_5.R
+++ ASSIGNMENT_1-GROUP_5.R
@@ -1,9 +1,29 @@
-#Start with this Always for Shiny
+#LOAD R PACKAGES
 library(shiny)
+library(shinythemes)
 
 # UI
 
+ui <- fluidPage(theme=shinytheme("superhero"),
+                
+                titlePanel("Lab Exercise 1: Association Rule Mining/Recommender System"),
+                
+                sidebarLayout(
+                  
+                  sidebarPanel(), 
+                  
+                  mainPanel(
+                    tabsetPanel(
+                      tabPanel("Page 1"), 
+                      tabPanel("Page 2"), 
+                      tabPanel("Page 3")
+                    )
+                  )
+                )
+)
 
 # SERVER
+server <- function(input, output, session) { }
 
+#LAUNCH SHINY OBJECT
 shinyApp(ui = ui, server = server)
